38d4be3404c5b224fd47033ec180aed51f5f1cb8
rifan1013 <rifan7362@gmail.com>
2021-01-20 09:24:14 +0530

    core practice

---
+++ EF-Core-Code-First-main/BookStoresWebAPI/BookStoresWebAPI/Models/BookStoresDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoresWebAPI.Models
+{
+    public class BookStoresDBContext : DbContext
+    {
+        public DbSet<Publisher> Publishers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-BEJH8GV;Initial Catalog=BookStoresDB;Integrated Security=True");
+        }
+    }
+}
